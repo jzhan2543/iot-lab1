f75a657768fd98af0eb66a4aad8933804fe7e905
Sam <40672605+parmsam@users.noreply.github.com>
2022-01-25 19:00:17 -0500

    random direction obstacle avoidance script
    need to test this on pi

---
+++ obstacle_avoidance2.py
@@ -0,0 +1,32 @@
+import picar_4wd as fc
+import random
+
+speed = 30
+
+def turn_backwards(speed):
+  fc.turn_right(speed)
+  fc.turn_right(speed)
+  
+def main():
+    while True:
+        scan_list = fc.scan_step(35)
+        
+        if not scan_list:
+            continue
+            
+        tmp = scan_list[3:7]
+        print(tmp)
+        
+        if tmp != [2,2,2,2]:
+            list_of_directions = [fc.turn_right, fc.turn_left, turn_backwards]
+            random.choice(list_of_directions)(speed)
+        else:
+            fc.forward(speed)
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        fc.stop()
+
+main()
