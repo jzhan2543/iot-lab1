d29e63a1b8b2e6f46603b837ceeb3bfea184d19f
Sam <40672605+parmsam@users.noreply.github.com>
2022-02-10 22:42:16 -0600

    added explanation comments

--- obstacle_avoidance2.py
+++ obstacle_avoidance2.py
@@ -6,24 +6,27 @@ speed = 30
 
 def turn_backwards(speed):
   fc.turn_right(speed)
-  fc.turn_right(speed+20)
+  fc.turn_right(speed)
   
 def main():
     while True:
+        #scan_step using default value of 35 cm distance
+        #scan_list is an array of 10 status readings (at 18 degree increments each) with values of 2 (greater than ref distance), 1 (if between 10 and ref), or 0 (if lower than ref)
         scan_list = fc.scan_step(35)
-        
+        #check if array is empty
         if not scan_list:
             continue
-            
+        #slicing values from position 3 to 7
         tmp = scan_list[3:7]
         print(tmp)
-        
+        #if at or less than ref distance then trigger condition to stop, go backwards, sleep, and pick a random direction to move in
         if tmp != [2,2,2,2]:
             fc.stop()
-            fc.backward(speed+20)
+            fc.backward(speed)
             time.sleep(0.5)
             list_of_directions = [fc.turn_right, fc.turn_left, turn_backwards]
             random.choice(list_of_directions)(speed)
+        #else keep moving forward
         else:
             fc.forward(speed)
 
